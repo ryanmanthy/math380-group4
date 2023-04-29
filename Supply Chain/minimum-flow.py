bd6bbd28c9df6308ea53325b3545e5370175cbd2
ttan6 <ttan6@hawk.iit.edu>
2023-04-29 17:16:45 -0500

    changed example values

--- Supply Chain/minimum-flow.py
+++ Supply Chain/minimum-flow.py
@@ -7,12 +7,12 @@ def main():
     # Defining variables
     cap_a, cap_b, cap_c, cap_d, cap_e, cap_f, cap_g, cap_h, cap_i = 75, 60, 87, 54, 92, 90, 38, 87, 49
     cost_a, cost_b, cost_c, cost_d, cost_e, cost_f, cost_g, cost_h, cost_i = 3, 4, 1, 2, 7, 2, 6, 2, 5
-    sup_0, sup_1, sup_2, sup_3, sup_4 = 60, 40, 0, -45, -55
+    sup_0, sup_1, sup_2, sup_3, sup_4 = 100, 0, 0, -45, -55
 
     # Defining arrays: sources, destinations, capacities, and unit costs between each pair
     # For example, the capacity from node 0 to node 2 is 75 and the cost is 3 per unit
     start_nodes = np.array([0, 0, 1, 1, 1, 2, 2, 3, 4])
-    end_nodes = np.array([2, 3, 2, 3, 4, 3, 4, 4, 2])
+    end_nodes = np.array([1, 2, 2, 3, 4, 3, 4, 4, 2])
     capacities = np.array([cap_a, cap_b, cap_c, cap_d, cap_e, cap_f, cap_g, cap_h, cap_i])
     unit_costs = np.array([cost_a, cost_b, cost_c, cost_d, cost_e, cost_f, cost_g, cost_h, cost_i])
 
